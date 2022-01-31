cb386e7be126aa0ea48db3757c5b065c8174ac34
mfadlanridho <devmfadlanridho@gmail.com>
2022-01-31 19:12:27 +0700

    initial commit

---
+++ event-bus/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json())
+
+const events = [];
+
+app.get('/events', (req, res) => {
+  res.send(events);
+})
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  events.push(event);
+
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post('http://localhost:4003/events', event).catch((err) => {
+    console.log(err.message);
+  });
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log('Listening on 4005');
+});
