da1b5e0947b847de5454e7aca85bfaf75e811d75
mfadlanridho <devmfadlanridho@gmail.com>
2022-02-03 09:06:17 +0700

    added k8s infrastructure

--- comments/index.js
+++ comments/index.js
@@ -22,7 +22,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'CommentCreated',
     data: {
       id: commentId,
